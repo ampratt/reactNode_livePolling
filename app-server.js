cef459ccbc3c8311752567c70f8e240312302ff5
Aaron <pratt.aaron.m@gmail.com>
2017-04-27 16:07:35 +0300

    socket connection established on client and server

--- app-server.js
+++ app-server.js
@@ -8,6 +8,14 @@ app.use(express.static('./public'))
 app.use(express.static('./node_modules/bootstrap/dist'))
 
 // port
-app.listen(3000)
+let server = app.listen(3000)
+
+// socket server to also listen on port 3000
+let io = require('socket.io').listen(server)
+
+// event hanler for socket connect
+io.sockets.on('connection', (socket) => {
+	console.log("Connected: %s", socket.id)
+})
 
 console.log("Polling server is running at 'http://localhost:3000'")
--- components/App.js
+++ components/App.js
@@ -1,6 +1,19 @@
 import React from 'react'
+import io from 'socket.io-client'
+
 
 const App = React.createClass({
+
+	componentWillMount() {
+		this.socket = io('http://localhost:3000')
+		// listener for connect event
+		this.socket.on('connect', this.connect)
+	},
+
+	connect() {
+		alert("Connected: " + this.socket.id)
+	},
+
 	render(){
 		return (
 			<h1>Hello from React!</h1>	
